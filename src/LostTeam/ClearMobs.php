6b72341a62e678fb2184083fd9dafd06ed626bca
jasonwynn10 <jasonwynn10@gmail.com>
2016-04-29 21:59:06 -0400

    Final Update for release

--- src/LostTeam/ClearMobs.php
+++ src/LostTeam/ClearMobs.php
@@ -10,13 +10,45 @@ use pocketmine\Player;
 
 
 class ClearMobs extends PluginBase {
+  public function $Title = (TF::BLUE."[".TF::GREEN."".TF::BLUE."]".TF::YELLOW." ");
   public function onEnable() {
     $this->getLogger()->notice(TF::GREEN."Enabled!");
   }
-  public function onCommand(CommandSender $s, Command $cmd, $list, array $args) {
-    if(strtolower($cmd) === "cm") {
-      //
+  public function onCommand(CommandSender $sender, Command $command, $list, array $args) {
+    if(strtolower($command) === "cm" or strtolower($command) === "clearmobs") {
+      $Count = $this->removeMobs();
+      $sender->sendMessage($this->Title."Removed ".$Count." mobs!");
+      return true;
     }
+    if(strtolower($command) === "ca" or strtolower($command) === "clearall") {
+      $Count = $this->removeEntities();
+      $sender->sendMessage($this->Title."Removed ".$Count." entities!");
+      return true;
+    }
+  }
+  public function removeEntities() {
+    $i = 0;
+    foreach($this->getServer()->getLevels() as $level) {
+      foreach($level->getEntities() as $entity) {
+        if(!$entity instanceof Player) {
+          $entity->close();
+          $i++;
+        }
+      }
+    }
+    return $i;
+  }
+  public function removeMobs() {
+    $i = 0;
+    foreach($this->getServer()->getLevels() as $level) {
+      foreach($level->getEntities() as $entity) {
+        if(!$entity instanceof Player or !$entity instanceof DroppedItem) {
+          $entity->close();
+          $i++;
+        }
+      }
+    }
+    return $i;
   }
   public function onDisable() {
     $this->getLogger()->notice(YF::GREEN."Disabled!");
