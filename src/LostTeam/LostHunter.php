151ed3316b9503379dc4f6234313d2d96fa10db6
jasonwynn10 <jasonwynn10@gmail.com>
2016-06-07 09:07:56 -0400

    changed from ClearMobs to LostHunter

--- src/LostTeam/LostHunter.php
+++ src/LostTeam/LostHunter.php
@@ -5,44 +5,26 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as TF;
-use pocketmine\entity\Item;
 use pocketmine\Player;
 
-
-class ClearMobs extends PluginBase {
+class LostHunter extends PluginBase {
   public $Title = (TF::BLUE."[".TF::GREEN."ClearMobs".TF::BLUE."]".TF::YELLOW." ");
   public function onEnable() {
     $this->getLogger()->notice(TF::GREEN."Enabled!");
   }
   public function onCommand(CommandSender $sender, Command $command, $list, array $args) {
-    if(strtolower($command) === "cm" or strtolower($command) === "clearmobs") {
-      $Count = $this->removeMobs();
-      $sender->sendMessage($this->Title."Removed ".$Count." mobs!");
-      return true;
-    }
-    if(strtolower($command) === "ca" or strtolower($command) === "clearall") {
+    if(strtolower($command) === "h" or strtolower($command) === "hunt") {
       $Count = $this->removeEntities();
       $sender->sendMessage($this->Title."Removed ".$Count." entities!");
       return true;
     }
+    return false;
   }
   public function removeEntities() {
     $i = 0;
     foreach($this->getServer()->getLevels() as $level) {
       foreach($level->getEntities() as $entity) {
         if(!$entity instanceof Player) {
-          $entity->close();
-          $i++;
-        }
-      }
-    }
-    return $i;
-  }
-  public function removeMobs() {
-    $i = 0;
-    foreach($this->getServer()->getLevels() as $level) {
-      foreach($level->getEntities() as $entity) {
-        if(!$entity instanceof Player and !$entity instanceof Item) {
           $entity->close();
           $i++;
         }
